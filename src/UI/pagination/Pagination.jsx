76d1c51c5d0057b9d27ba1fc49aca7deb2e6ebc1
Igorindaction <54639505+Igorindaction@users.noreply.github.com>
2021-09-16 01:31:26 +0300

    initializeInfiniteScroll

--- src/UI/pagination/Pagination.jsx
+++ src/UI/pagination/Pagination.jsx
@@ -2,13 +2,15 @@ import IteratingPages from 'hooks/iteratingPages';
 import React from 'react';
 import ButtonDefault from 'UI/button/ButtonDefault';
 
-const Pagination = ({currentPage, setCurrentPage, totalPages}) => {
+const Pagination = ({page, changePage, totalPages}) => {
+    
     const totalPagesArray = IteratingPages(totalPages)
+    console.log(`Общение количество страниц = ${totalPagesArray}`)
     return (
         <div className="button-wrapper">
             {
                 totalPagesArray.map((p, index) => 
-                <ButtonDefault key={index} onClick={(() => setCurrentPage(p))}  className={currentPage === p ? 'currentPage' : 'buttonDefault' }>{p}</ButtonDefault>
+                <ButtonDefault key={index} onClick={(() => changePage(p))}  className={page === p ? 'currentPage' : 'buttonDefault' }>{p}</ButtonDefault>
                 )
             }
         </div>
--- src/pages/posts/posts.jsx
+++ src/pages/posts/posts.jsx
@@ -1,6 +1,6 @@
 
 import { useSortedAndFilteredPosts } from "hooks/usePost";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ButtonDefault from "UI/button/ButtonDefault";
 import ModalWindow from "UI/modalWindow/ModalWindow";
 import PostFilter from "../../Components/postFilter/PostFilter";
@@ -11,6 +11,8 @@ import Loader from "UI/loader/Loader";
 import { useFetching } from "hooks/useFetching";
 import { getPageCount } from "API/utils/pages";
 import Pagination from "UI/pagination/Pagination";
+import { useObserver } from "hooks/useObserver";
+import SelectDefault from "UI/select/SelectDefault";
 
 
 function Posts() {
@@ -22,22 +24,28 @@ function Posts() {
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
   const [currentPage, setCurrentPage] = useState(1)
-  
+  const lastElement = useRef()
+  const observer = useRef()
 
 
   const sortedAndSearchedPosts = useSortedAndFilteredPosts(posts, filter.sort, filter.query)
 
-  const [getPostsFromJson, isLoading, postError] = useFetching( async() => {
-    const response = await PostService.getAll(currentPage, limit)
-    setPosts(response.data)
+  const [getPostsFromJson, isPostLoading, postError] = useFetching( async() => {
+    const response = await PostService.getAll(page, limit)
+    setPosts([...posts, ...response.data])
     const totalCount = (response.headers['x-total-count'])
     setTotalPages(getPageCount(totalCount, limit))
+    console.log(totalPages);
   })
 
+  useObserver(lastElement, page < totalPages, isPostLoading, (() => {
+      setPage(page + 1)
+  }))
+
   useEffect(() => {
-    console.log('сработала юзэффект')
-    getPostsFromJson()
-  }, [currentPage])  
+    console.log('сработало');
+    getPostsFromJson(page, limit)
+  }, [page, limit])  
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -48,17 +56,32 @@ function Posts() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const changePage = (page) => {
+    console.log(page)
+    setPage(page)
+  }
+
 
   return (
     <div className="App">
       {postError && <div>Произошла ошбика {postError}</div>}
       <ButtonDefault onClick={() => setModal(true)}>Создать посты</ButtonDefault>
+      <SelectDefault value={limit} onChange={ value => setLimit(value)} defaultName='Количество элементов на странице' options={[
+
+        {value: 5, name: '5'},
+        {value: 10, name: '10'},
+        {value: 25, name: '25'},
+        {value: -1, name: 'Загрузить все'},
+      ]
+      }/>
       <ModalWindow visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </ModalWindow>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      {isLoading ? <Loader /> : <PostList sortedAndSearchedPosts={sortedAndSearchedPosts} removePost={removePost}/> }
-      <Pagination  currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages}/>
+      <PostList sortedAndSearchedPosts={sortedAndSearchedPosts} removePost={removePost}/> 
+      <div ref={lastElement} style={{height: '20px', background: 'red'}}></div>
+      {isPostLoading && <Loader />}
+      <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
     </div>
   );
 }
