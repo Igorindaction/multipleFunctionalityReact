22080cbd3303c1e1de2747083aa5036e9980314e
Igorindaction <54639505+Igorindaction@users.noreply.github.com>
2021-09-19 12:36:00 +0300

    CRAProjectV1.2

--- src/pages/postInfo/postInfo.jsx
+++ src/pages/postInfo/postInfo.jsx
@@ -29,20 +29,20 @@ const PostInfo = () => {
 
     return (
         <div>
-            <h1>Вы перешли на страницу юзера = {params.id}</h1>
+            <h1 style={{textAlign: "center", marginTop: "10px"}}>Вы перешли на страницу юзера {params.id}</h1>
             {isLoading 
                 ? <Loader/> 
                 : <div>
-                    <h2 style={{'marginTop': '20px'}}>Информация:</h2>
-                    <p><strong>Заголовок</strong> - {post.title}</p>
-                    <p><strong>Содержание</strong> - {post.body}</p>
+                    <h2 style={{'marginTop': '20px', marginBottom: "15px" }}>Информация:</h2>
+                    <p><strong>Заголовок</strong>: {post.title}</p>
+                    <p><strong>Содержание</strong>: {post.body}</p>
                  </div> 
             }
             {isComLoading 
                 ? <Loader/> 
-                : <div style={{'marginTop': '20px'}}>
+                : <div style={{'marginTop': '20px'}}><h3 style={{marginBottom: "15px"}}>Комментарии:</h3>
                 {comments.map( (comm, index) => 
-                    <div key={comm.email}>
+                    <div key={comm.email} style={{marginBottom: "8px"}}>
                         <h5 key={comm.id}>{comm.email}</h5>
                         <p key={comm.index}>{comm.name}</p>
                     </div>
